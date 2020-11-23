4f17c02a8aade9fabe9090d92fcc4f1881235893
Daniel Bisyukov <07.jan.2002@gmail.com>
2020-11-23 20:52:57 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button__next',
+    prevEl: '.slider-button__prev',
+  },
+
+  effect: "cube",
+})
